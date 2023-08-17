5c75e15e6c3cce5d1d717da29d032f6c5c651b24
TSOlami <tijani.saheed@yahoo.com>
2023-08-17 17:16:58 +0100

    add a calender for python libraries practice

---
+++ exercise.py
@@ -0,0 +1,12 @@
+from calendar import *
+
+
+year = int(input('Enter Year: '))
+
+print(calendar(year, 3, 1, 8, 3))
+
+#2 = 2 characters for days (Mo, Tu, etc)
+#1 = 1 line (row) for each week
+#8 = 8 rows for each month
+#3 = 3 columns for all months of the year
+#TSOlami
